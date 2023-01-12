a7f91b1bb44820e1862ea309ed97ea40ae7a851a
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-01-12 10:12:02 +0530

    Create Pizza,Pizza.cpp

---
+++ Pizza,Pizza.cpp
@@ -0,0 +1,22 @@
+// Date -> 12th January 2022
+// Code By -> Abhinav Anand
+
+
+// Problem Link -> https://codeforces.com/contest/979/problem/A
+
+// JAI SHREE KRISHNA
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n;
+    cin>>n;
+    n+=1;
+    if(n==1)
+        cout << 0 << endl;
+    else if(n%2==0)
+        cout << n/2 << endl;
+    else
+        cout << n << endl;
+}
