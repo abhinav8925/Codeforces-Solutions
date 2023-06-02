b3b6db76b0c92d82a39d707681bd604427734679
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-06-03 00:59:58 +0530

    Create  Football.cpp

---
+++ Football.cpp
@@ -0,0 +1,116 @@
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date - 3rd June 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://codeforces.com/problemset/problem/43/A
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+int N=1000001;
+vector<int> arr(N,true);
+vector<int> prime_num;
+vector<int> prime_fact(N,-1);
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+
+void sieve_prime_factor()
+{
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(prime_fact[i]==-1)
+        {
+            for(int j=i;j<=N;j+=i)
+            {
+                // if(prime_fact[j]==-1)
+                    prime_fact[j]=i;
+            }
+        }
+    }
+    
+    int k;
+    cin>>k;
+    while(k>1)
+    {
+        cout << prime_fact[k] << " ";
+        k=k/prime_fact[k];
+    }
+}
+
+bool isprime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+
+void solve()
+{
+ 
+    
+    int n;
+    cin>>n;
+    map<string,int> m;
+    while(n--)
+    {
+        string s;
+        cin>>s;
+        m[s]++;
+    }
+    int mx=0;
+    string ans;
+    for(auto &it:m)
+    {
+        mx=max(mx,it.second);
+        if(mx==it.second){
+            ans=it.first;
+        }
+    }
+    
+    cout << ans << endl;
+    
+    
+}
+
+int main() {
+	
+	//sieve_prime_factor();
+	// sieve();
+// 	int t;
+// 	cin>>t;
+// 	while(t--)
+// 	{
+		solve();
+// 	}
+	return 0;
+}
