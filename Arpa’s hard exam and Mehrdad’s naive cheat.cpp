1dd407df7038bc50b1b5129c3b6fdb76574dca07
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-06-20 02:30:34 +0530

    Create Arpa’s hard exam and Mehrdad’s naive cheat.cpp

---
+++ Arpa’s hard exam and Mehrdad’s naive cheat.cpp
@@ -0,0 +1,110 @@
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date -> 20th June 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://codeforces.com/problemset/problem/742/A
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+int N=1000001;
+vector<int> arr(N,true);
+vector<int> prime_num;
+vector<int> prime_fact(N,-1);
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+
+void sieve_prime_factor()
+{
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(prime_fact[i]==-1)
+        {
+            for(int j=i;j<=N;j+=i)
+            {
+                // if(prime_fact[j]==-1)
+                    prime_fact[j]=i;
+            }
+        }
+    }
+    
+    int k;
+    cin>>k;
+    while(k>1)
+    {
+        cout << prime_fact[k] << " ";
+        k=k/prime_fact[k];
+    }
+}
+
+bool isprime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+
+void solve()
+{
+    // ios_base::sync_with_stdio(0);
+    // cout.precision(7);
+    int n,a=8;
+    cin>>n;
+    int res=1;
+    while(n!=0){
+        if(n%2){
+            res=(res%10 * a%10)%10;
+            n--;
+        }
+        else{
+           a=(a%10 * a%10)%10;
+           n/=2;
+        }
+    }
+    
+    cout << res%10 << endl;
+    
+}
+
+int main() {
+	
+	//sieve_prime_factor();
+	// sieve();
+// 	int t;
+// 	cin>>t;
+// 	while(t--)
+// 	{
+		solve();
+// 	}
+	return 0;
+}
