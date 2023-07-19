acaae1a120984de878dc8a119701d088431873e9
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-07-19 23:20:24 +0530

    Create 1183A - Nearest Interesting Number .cpp

---
+++ CF 800 (Implementation,Brute Force)/1183A - Nearest Interesting Number .cpp
@@ -0,0 +1,115 @@
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date -> 19th July 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://codeforces.com/problemset/problem/1183/A
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+int N=1000001;
+vector<int> arr(N,true);
+vector<int> prime_num;
+vector<int> prime_fact(N,-1);
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+
+void sieve_prime_factor()
+{
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(prime_fact[i]==-1)
+        {
+            for(int j=i;j<=N;j+=i)
+            {
+                // if(prime_fact[j]==-1)
+                    prime_fact[j]=i;
+            }
+        }
+    }
+    
+    int k;
+    cin>>k;
+    while(k>1)
+    {
+        cout << prime_fact[k] << " ";
+        k=k/prime_fact[k];
+    }
+}
+
+bool isprime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+int sum(int n){
+    int sum=0;
+    while(n>0){
+        sum+=(n%10);
+        n/=10;
+    }
+    return sum;
+}
+
+void solve()
+{
+    // ios_base::sync_with_stdio(0);
+    // cout.precision(7);
+    
+    int n;
+    cin>>n;
+    
+    while(sum(n)%4!=0){
+        n++;
+    }
+    
+    cout << n << endl;
+    
+}
+
+int main() {
+	
+	//sieve_prime_factor();
+	// sieve();
+	/*int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}*/
+	
+	solve();
+	
+	return 0;
+}
