afcb83dcf21c2c31c42b8403bcdf6e1ac94ac5fb
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-07-27 18:34:48 +0530

    Create 1535B - Array Reodering .cpp

---
+++ 1535B - Array Reodering .cpp
@@ -0,0 +1,119 @@
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date -> 27th July 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://codeforces.com/problemset/problem/1535/B
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+int N=1000001;
+int mod=1000000007;
+vector<int> arr(N,true);
+vector<int> prime_num;
+vector<int> prime_fact(N,-1);
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+
+void sieve_prime_factor()
+{
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(prime_fact[i]==-1)
+        {
+            for(int j=i;j<=N;j+=i)
+            {
+                // if(prime_fact[j]==-1)
+                    prime_fact[j]=i;
+            }
+        }
+    }
+    
+    int k;
+    cin>>k;
+    while(k>1)
+    {
+        cout << prime_fact[k] << " ";
+        k=k/prime_fact[k];
+    }
+}
+
+bool isprime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+
+void solve()
+{
+    // ios_base::sync_with_stdio(0);
+    // cout.precision(7);
+    ll n;
+    cin >> n;
+    ll a[n]; 
+    
+    int ce=0;
+    for (ll i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    sort(a,a+n,[](int x,int y){
+        return x%2<y%2;
+    });
+
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+                if(__gcd(a[i],(2*a[j]))>1)
+                    ce++;
+        }
+    }
+
+    cout << ce << endl;
+    
+    
+    
+}
+
+int main() {
+	
+	//sieve_prime_factor();
+	// sieve();
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
+}
