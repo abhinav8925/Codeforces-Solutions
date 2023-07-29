6b53501021e3dd034085f5aa74bd87534733fb83
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-07-29 19:30:28 +0530

    Create 1560B - Who's Opposite? .cpp

---
+++ 1560B - Who's Opposite? .cpp
@@ -0,0 +1,110 @@
+
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date - 29th July 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://codeforces.com/problemset/problem/1560/B
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+int N=1000001;
+vector<int> arr(N,true);
+vector<int> prime_num;
+vector<int> prime_fact(N,-1);
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+
+void sieve_prime_factor()
+{
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(prime_fact[i]==-1)
+        {
+            for(int j=i;j<=N;j+=i)
+            {
+                // if(prime_fact[j]==-1)
+                    prime_fact[j]=i;
+            }
+        }
+    }
+    
+    int k;
+    cin>>k;
+    while(k>1)
+    {
+        cout << prime_fact[k] << " ";
+        k=k/prime_fact[k];
+    }
+}
+
+bool isprime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+
+void solve()
+{
+    ll a,b,c;
+    cin>>a>>b>>c;
+
+    ll n=2*abs(a-b);
+
+    if(a>n || b>n || c>n)
+        cout << -1 << endl;
+    else{
+        ll d=c+(n/2);
+        while(d>n)
+            d-=n;
+        
+
+        cout << d << endl;
+    }
+    
+}
+
+int main() {
+	
+	//sieve_prime_factor();
+	// sieve();
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+    // solve();
+	return 0;
+}
