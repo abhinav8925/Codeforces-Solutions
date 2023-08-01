ac530318d75be13496518a7f69986b8d36f076fa
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-08-01 11:15:06 +0530

    Create 34A - Reconnaissance 2 .cpp

---
+++ 34A - Reconnaissance 2 .cpp
@@ -0,0 +1,135 @@
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date -> 1st August 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://codeforces.com/problemset/problem/34/A
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+int N=1000001;
+int mod=1000000007;
+vector<int> arr(N,true);
+vector<int> prime_num;
+vector<int> prime_fact(N,-1);
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+
+void sieve_prime_factor()
+{
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(prime_fact[i]==-1)
+        {
+            for(int j=i;j<=N;j+=i)
+            {
+                // if(prime_fact[j]==-1)
+                    prime_fact[j]=i;
+            }
+        }
+    }
+    
+    int k;
+    cin>>k;
+    while(k>1)
+    {
+        cout << prime_fact[k] << " ";
+        k=k/prime_fact[k];
+    }
+}
+
+bool isprime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+ll fact(ll n){
+    if(n==0)
+        return 1;
+    return n*fact(n-1);
+}
+ll sum(ll n) {
+    ll sum = 0;
+    while (n != 0) {
+        sum += (n % 10);
+        n /= 10;
+    }
+    return sum;
+}
+
+void solve()
+{
+    
+    // cout.precision(7);
+    ll n;
+    cin>>n;
+    ll a[n];
+    for(ll i=0;i<n;i++)
+        cin>>a[i];
+    
+    // sort(a,a+n);
+    ll x=100,y=100;
+    ll ans=10000;
+    int i;
+    for( i=0;i<n-1;i++){
+        ans=min(ans,abs(a[i]-a[i+1]));
+        if(ans==abs(a[i]-a[i+1])){
+            x=i,y=i+1;
+        }
+    }
+    ans=min(ans,abs(a[n-1]-a[0]));
+    if(ans==abs(a[n-1]-a[0]))
+        cout << 1 << " " << n << endl;
+    else
+        cout << x+1 << " " << y+1 << endl;
+}
+
+int main() {
+	
+	//sieve_prime_factor();
+	// sieve();
+	ios_base::sync_with_stdio(0);
+    cin.tie(0); 
+    cout.tie(0);
+// 	int t;
+// 	cin>>t;
+// 	while(t--)
+// 	{
+// 		solve();
+// 	}
+	solve();
+	return 0;
+
+
+}
