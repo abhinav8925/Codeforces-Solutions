a71c5625a0f21d0ade5d72792dcebac5e0d9054b
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-08-09 05:36:02 +0530

    Create 1837B - Comparison String.cpp

---
+++ 1837B - Comparison String.cpp
@@ -0,0 +1,133 @@
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date -> 9th August 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://codeforces.com/problemset/problem/1837/B
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+int N=1000001;
+int mod=1000000007;
+vector<int> arr(N,true);
+vector<int> prime_num;
+vector<int> prime_fact(N,-1);
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+
+void sieve_prime_factor()
+{
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(prime_fact[i]==-1)
+        {
+            for(int j=i;j<=N;j+=i)
+            {
+                // if(prime_fact[j]==-1)
+                    prime_fact[j]=i;
+            }
+        }
+    }
+    
+    int k;
+    cin>>k;
+    while(k>1)
+    {
+        cout << prime_fact[k] << " ";
+        k=k/prime_fact[k];
+    }
+}
+
+bool isprime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+ll fact(ll n){
+    if(n==0)
+        return 1;
+    return n*fact(n-1);
+}
+ll sum(ll n) {
+    ll sum = 0;
+    while (n != 0) {
+        sum += (n % 10);
+        n /= 10;
+    }
+    return sum;
+}
+
+
+void solve()
+{
+    
+    // cout.precision(7);
+    
+    ll n;
+    cin>>n;
+    string s;
+    cin>>s;
+    ll mx=1,cur=1;
+    for(ll i=0;i<n-1;i++){
+        if(s[i]==s[i+1]){
+            cur++;
+        }
+        else{
+            mx=max(mx,cur);
+            cur=1;
+        }
+    }
+    
+    cout << max(mx,cur)+1 << endl;
+}
+
+
+int main() {
+	
+	//sieve_prime_factor();
+	// sieve();
+	ios_base::sync_with_stdio(0);
+    cin.tie(0); 
+    cout.tie(0);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+// 	solve();
+	return 0;
+
+
+}
