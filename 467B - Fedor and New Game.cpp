7106adcb5dd952f4f067e8d0893ea6d910eebd3a
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-08-28 12:26:33 +0530

    Create 467B - Fedor and New Game.cpp

---
+++ 467B - Fedor and New Game.cpp
@@ -0,0 +1,130 @@
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date -> 28th August 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://codeforces.com/problemset/problem/467/B
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define nl "\n"
+using namespace std;
+int N=1000001;
+int mod=1000000007;
+vector<int> arr(N,true);
+vector<int> prime_num;
+vector<int> prime_fact(N,-1);
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+
+void sieve_prime_factor()
+{
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(prime_fact[i]==-1)
+        {
+            for(int j=i;j<=N;j+=i)
+            {
+                // if(prime_fact[j]==-1)
+                    prime_fact[j]=i;
+            }
+        }
+    }
+    
+    int k;
+    cin>>k;
+    while(k>1)
+    {
+        cout << prime_fact[k] << " ";
+        k=k/prime_fact[k];
+    }
+}
+ 
+bool isprime(ll n)
+{
+    for(ll i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+
+void solve()
+{
+    // cout.precision(7);
+    
+    int n,m,k;
+    cin>>n>>m>>k;
+    int a[m+1];
+    for(int i=0;i<m+1;i++)
+        cin>>a[i];
+    
+    int mx= a[m];
+    
+    bitset<30> b1(mx);
+    
+    int ans=0;
+    
+    
+    for(int i=0;i<m;i++){
+        bitset<30> z(a[i]);
+        int ch=0;
+        for(int j=0;j<30;j++){
+            if(b1[j]!=z[j])
+                ch++;
+        }
+        
+        if(ch<=k)
+            ans++;
+    }
+    
+    cout << ans << endl;
+    
+    
+    
+}
+    
+int main() {
+	
+	//sieve_prime_factor();
+	// sieve();
+	ios_base::sync_with_stdio(0);
+    cin.tie(0); 
+    cout.tie(0);
+// 	int t;
+// 	cin>>t;
+// 	while(t--)
+// 	{
+// 		solve();
+// 	}
+	solve();
+	return 0;
+
+
+}
