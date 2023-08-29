10ddcaf3f54fa7bbeda43a700943b626b40356c2
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-08-29 13:34:47 +0530

    Create 	82A - Double Cola.cpp

---
+++ 82A - Double Cola.cpp
@@ -0,0 +1,113 @@
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date -> 29th August 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://codeforces.com/problemset/problem/82/A
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define nl "\n"
+using namespace std;
+int N=1000001;
+int mod=1000000007;
+vector<int> arr(N,true);
+vector<int> prime_num;
+vector<int> prime_fact(N,-1);
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+
+void sieve_prime_factor()
+{
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(prime_fact[i]==-1)
+        {
+            for(int j=i;j<=N;j+=i)
+            {
+                // if(prime_fact[j]==-1)
+                    prime_fact[j]=i;
+            }
+        }
+    }
+    
+    int k;
+    cin>>k;
+    while(k>1)
+    {
+        cout << prime_fact[k] << " ";
+        k=k/prime_fact[k];
+    }
+}
+ 
+bool isprime(ll n)
+{
+    for(ll i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+
+
+void solve()
+{
+    // cout.precision(7);
+    
+    ll n;
+    cin>>n;
+    string a[5]={"Sheldon","Leonard","Penny","Rajesh","Howard"};
+    ll k=1;
+    while((k*5)<n){
+        n-=(k*5);
+        k*=2;
+    }
+    
+    cout << a[(n-1)/k] << nl;
+    
+}
+    
+int main() {
+	
+	//sieve_prime_factor();
+	// sieve();
+	ios_base::sync_with_stdio(0);
+    cin.tie(0); 
+    cout.tie(0);
+	/*int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}*/
+	solve();
+	return 0;
+
+
+}
