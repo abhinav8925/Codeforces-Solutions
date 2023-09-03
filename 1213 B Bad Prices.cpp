ac293d5d4a73c154ad9eb6ebe6e4501a57775659
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-09-03 17:29:08 +0530

    Create 1213 B Bad Prices.cpp

---
+++ 1213 B Bad Prices.cpp
@@ -0,0 +1,126 @@
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date -> 3rd September 2023
+  Code by - Abhinav Anand
+
+  Problem link ->  https://codeforces.com/problemset/problem/1213/B
+  
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define nl "\n"
+using namespace std;
+int N=1000001;
+int mod=1000000007;
+vector<int> arr(N,true);
+vector<int> prime_num;
+vector<int> prime_fact(N,-1);
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+
+void sieve_prime_factor()
+{
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(prime_fact[i]==-1)
+        {
+            for(int j=i;j<=N;j+=i)
+            {
+                // if(prime_fact[j]==-1)
+                    prime_fact[j]=i;
+            }
+        }
+    }
+    
+    int k;
+    cin>>k;
+    while(k>1)
+    {
+        cout << prime_fact[k] << " ";
+        k=k/prime_fact[k];
+    }
+}
+ 
+bool isprime(ll n)
+{
+    for(ll i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+
+
+
+ 
+
+void solve()
+{
+    // cout.precision(7);
+    
+    int n;
+    cin>>n;
+    
+    int a[n];
+    // map<int,int> m;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        // m[a[i]]=i;
+    }
+    int cnt=0;
+    int mn=INT_MAX;
+    for(int i=n-1;i>=0;i--){
+        if(a[i]>mn)
+            cnt++;
+        mn=min(mn,a[i]);
+    }
+    
+    cout << cnt << endl;
+    
+    
+    
+}
+    
+int main() {
+	
+	//sieve_prime_factor();
+	// sieve();
+	ios_base::sync_with_stdio(0);
+    cin.tie(0); 
+    cout.tie(0);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+// 	solve();
+	return 0;
+
+
+}
