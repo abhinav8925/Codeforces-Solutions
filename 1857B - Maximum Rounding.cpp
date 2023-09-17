7d34798ba053a3cea41f9bd2775db7e169675c3a
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-09-17 23:11:28 +0530

    Create 1857B - Maximum Rounding.cpp

---
+++ 1857B - Maximum Rounding.cpp
@@ -0,0 +1,111 @@
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date -> 17th September 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://codeforces.com/problemset/problem/1857/B
+  
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define nl "\n"
+using namespace std;
+int N=1000001;
+int mod=1000000007;
+vector<int> arr(N,true);
+vector<int> prime_num;
+vector<int> prime_fact(N,-1);
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+
+void sieve_prime_factor()
+{
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(prime_fact[i]==-1)
+        {
+            for(int j=i;j<=N;j+=i)
+            {
+                // if(prime_fact[j]==-1)
+                    prime_fact[j]=i;
+            }
+        }
+    }
+    
+    int k;
+    cin>>k;
+    while(k>1)
+    {
+        cout << prime_fact[k] << " ";
+        k=k/prime_fact[k];
+    }
+}
+ 
+bool isprime(ll n)
+{
+    for(ll i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+
+
+void solve(){
+    
+    string s;
+    cin>>s;
+    
+    s='0'+s;
+    
+    int n = s.size();
+    
+    for(int i=s.size()-1;i>=0;i--){
+        if(s[i]>='5')
+            s[i-1]++,n=i;
+    }
+    
+    for(int i=(s[0]=='0');i<s.size();i++){
+        cout << (i>=n?'0':s[i]);
+    }
+    
+    cout << "\n";
+}
+
+
+
+int main() {
+	
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+
+}
